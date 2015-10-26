9623e1d11003ae08a7686e1b347726ed9f68c37c
Nick Frost <nickfrostatx@gmail.com>
2015-10-26 12:09:15 -0700

    Do CI with Travis

--- {{cookiecutter.project_slug}}/tests/test_version.py
+++ {{cookiecutter.project_slug}}/tests/test_version.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-"""Test that the package exists and has specified metadata"""
+"""Test that the package exists and has specified metadata."""
 
-__author__ = '{{ cookiecutter.full_name }}'
-__copyright__ = 'Copyright {{ cookiecutter.year }}, {{ cookiecutter.legal_name }}'
-__version__ = '{{ cookiecutter.version }}'
-__email__ = '{{ cookiecutter.email }}'
+import {{ cookiecutter.project_slug }}
+
+
+def test_version():
+    assert {{ cookiecutter.project_slug }}.__version__ == '{{ cookiecutter.version }}'
